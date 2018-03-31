41360d4a7d379446b2c668987d59f0ee8a3b7fa9
Yang Zhou <yzhou3991@gmail.com>
2018-03-31 12:04:22 -0400

    customized rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: "portfolio_show"
 
   resources :blogs
 end
