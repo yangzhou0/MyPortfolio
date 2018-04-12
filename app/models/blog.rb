31776ff6f9fc96b81a7fe3a1e29d32c8b34cf987
Yang Zhou <yzhou3991@gmail.com>
2018-04-12 00:22:23 -0400

    add in enum functionality to update status for blogs

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,7 +1,7 @@
 class Blog < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
-
+  enum status: {draft: 0, published: 1}
   def should_generate_new_friendly_id?
     title_changed?
   end
