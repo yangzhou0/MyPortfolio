0e0a5ca34d09f3fd5fb73aec2f3736a608fc7055
Yang Zhou <yzhou3991@gmail.com>
2018-04-14 14:36:42 -0400

    add database relation between topics and blogs

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,8 +1,10 @@
 class Blog < ApplicationRecord
-  validates_presence_of :title, :body
+  validates_presence_of :title, :body, :topic_id
   extend FriendlyId
   friendly_id :title, use: :slugged
   enum status: {draft: 0, published: 1}
+
+  belongs_to :topic
   def should_generate_new_friendly_id?
     title_changed?
   end
