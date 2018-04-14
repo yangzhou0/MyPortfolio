227f2fe6ad2577768ffa753dddecf0b5cdc5190e
Yang Zhou <yzhou3991@gmail.com>
2018-04-14 16:40:40 -0400

    implement portfolio default image values using set_default method on model level

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,7 +1,14 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
+  after_initialize :set_default_image
 
   def self.react
     Portfolio.where(subtitle: 'React')
   end
+
+  def set_default_image
+    self.main_image ||= "http://via.placeholder.com/350x150"
+    self.thumb_image ||= 'http://via.placeholder.com/250x50'
+  end
+
 end
